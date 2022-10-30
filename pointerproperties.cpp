c1614b6753cec8dea05b0d6d29c018cfd9a363cd
gbstorey <gbstorey@asu.edu>
2022-10-30 16:45:10 -0700

    References

--- pointerproperties.cpp
+++ pointerproperties.cpp
@@ -255,11 +255,40 @@ using namespace std;
 //    return 0;
 //}
 
-int main() {
+// ----------------- REFERENCES ----------------- //
 
-    cout << endl;
-    return 0;
-}
+//int main() {
+//    int num {100};
+//    int &ref {num};
+//    cout << num << endl;
+//    cout << ref << endl;
+//
+//    num = 200;
+//    cout << "\n-----------------------" << endl;
+//    cout << num << endl;
+//    cout << ref << endl;
+//
+//    ref = 300;
+//    cout << "\n-----------------------" << endl;
+//    cout << num << endl;
+//    cout << ref << endl;
+
+//    vector <string> stooges {"Larry", "Moe", "Curly"};
+//    for (auto str: stooges)
+//            str = "Funny"; //str is a COPY of each vector element
+//    for (auto str: stooges) //warning thrown because copies are made for no reason
+//            cout << str << endl;
+//    cout << "\n--------------------------------" << endl;
+//    for (auto &str: stooges)
+//            str = "Funny";
+//    for (auto const &str:stooges)
+//            cout << str << endl; //vector elements have changed
+//    for (auto const &str: stooges)
+//            cout << str << endl;
+//
+//    cout << endl;
+//    return 0;
+//}
 
 
 #pragma clang diagnostic pop
