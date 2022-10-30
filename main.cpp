c371a26a8864cddfd2524febe10fb62c42f8739e
gbstorey <gbstorey@asu.edu>
2022-10-30 12:18:28 -0700

    Address; Initialization; Size

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "bugprone-sizeof-expression"
 // Section 12
 // Simple Pointers
 #include <iostream>
@@ -20,5 +22,20 @@ int main()
     p = nullptr;
     cout << "\nValue of p is: " << p << endl;
 
+    int *p1 {nullptr};
+    double *p2 {nullptr};
+    unsigned long long *p3 {nullptr};
+    vector<string> *p4{nullptr};
+    string *p5{nullptr};
+
+    cout << "\nsizeof p1 is:  " << sizeof p1 << endl;
+    cout << "sizeof p2 is: " << sizeof p2 << endl;
+    cout << "sizeof p3 is: " << sizeof p3 << endl;
+    cout << "sizeof p4 is: " << sizeof p4 << endl;
+    cout << "sizeof p5 is: " << sizeof p5 << endl;
+
     return 0;
 }
+
+
+#pragma clang diagnostic pop
