6ba8677d34ddec7d323fecacd225efbe22a12c8e
gbstorey <gbstorey@asu.edu>
2022-10-30 13:31:15 -0700

    Pointer Arithmetic

--- pointerproperties.cpp
+++ pointerproperties.cpp
@@ -103,27 +103,69 @@ int main()
 //    delete [] temp_ptr;
 
 // ----------------- ARRAYS AND POINTERS, SUBSCRIPT/OFFSET EQUIVALENCY ----------------- //
-    int scores[] {100, 95, 89};
-    cout << "Value of scores: " << scores << endl;
+//    int scores[] {100, 95, 89};
+//    cout << "Value of scores: " << scores << endl;
+//
+//    int *score_ptr {scores};
+//    cout << "Value of score_ptr: " << score_ptr << endl;
+//    cout << "\nArray subscript notation -------------------------" << endl;
+//    cout << scores[0] << endl;
+//    cout << scores[1] << endl;
+//    cout << scores[2] << endl;
+//    cout << "\nPointer subscript notation -------------------------" << endl;
+//    cout << score_ptr[0] << endl;
+//    cout << score_ptr[1] << endl;
+//    cout << score_ptr[2] << endl;
+//    cout << "\nPointer offset notation -----------------------------" << endl;
+//    cout << *score_ptr << endl;
+//    cout << *(score_ptr+1) << endl;
+//    cout << *(score_ptr+2) << endl;
+//    cout << "\nArray offset notation -----------------------------" << endl;
+//    cout << *scores << endl;
+//    cout << *(scores+1) << endl;
+//    cout << *(scores+2) << endl;
 
-    int *score_ptr {scores};
-    cout << "Value of score_ptr: " << score_ptr << endl;
-    cout << "\nArray subscript notation -------------------------" << endl;
-    cout << scores[0] << endl;
-    cout << scores[1] << endl;
-    cout << scores[2] << endl;
-    cout << "\nPointer subscript notation -------------------------" << endl;
-    cout << score_ptr[0] << endl;
-    cout << score_ptr[1] << endl;
-    cout << score_ptr[2] << endl;
-    cout << "\nPointer offset notation -----------------------------" << endl;
-    cout << *score_ptr << endl;
-    cout << *(score_ptr+1) << endl;
-    cout << *(score_ptr+2) << endl;
-    cout << "\nArray offset notation -----------------------------" << endl;
-    cout << *scores << endl;
-    cout << *(scores+1) << endl;
-    cout << *(scores+2) << endl;
+// ----------------- POINTER ARITHMETIC ----------------- //
+//    int scores[] {100, 95, 89, 68, -1}; //sentinel value -1
+//    int *score_ptr {scores};
+//
+//    while (*score_ptr != -1) {
+//        cout << *score_ptr << endl;
+//        score_ptr++;
+//    }
+//
+//    score_ptr = scores;
+//    while (*score_ptr != -1)
+//        cout << *score_ptr++ << endl; //equivalent style
+
+//    string s1{"Frank"};
+//    string s2{"Frank"};
+//    string s3{"James"};
+//
+//    string *p1 {&s1};
+//    string *p2 {&s2};
+//    string *p3 {&s1};
+//
+//    cout << boolalpha;
+//    cout << p1 << "==" << p2 << ": " << (p1 == p2) << endl; // false
+//    cout << p1 << "==" << p3 << ": " << (p1 == p3) << endl; // true
+//
+//    cout << *p1 << "==" << *p2 << ": " << (*p1 == *p2) << endl; // true
+//    cout << *p1 << "==" << *p3 << ": " << (*p1 == *p3) << endl; // true
+//
+//    p3 = &s3; //point to James
+//    cout << *p1 << "==" << *p3 << ": " << (*p1 == *p3) << endl; // false
+
+//    char name [] {"Frank"};
+//    char *char_ptr1 {nullptr};
+//    char *char_ptr2 {nullptr};
+//
+//    char_ptr1 = &name[0]; //F
+//    char_ptr2 = &name[3]; //N
+//
+//    cout << "In the string " << name << ", " << *char_ptr2 << " is " << (char_ptr2-char_ptr1) << " characters away from " << *char_ptr1 << endl;
+
+// ----------------- CONST AND POINTERS ----------------- //
 
 
 
